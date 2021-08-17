8827f136819970d15db9b19a9039b9d181685e22
kerins18 <kerins18@gmail.com>
2021-08-17 13:10:12 +0200

    setup files for pip packaging

--- setup.py
+++ setup.py
@@ -7,7 +7,11 @@ DESCRIPTION = 'Scrapeops Scrapy SDK (MVP)'
 
 setup(name='scrapeops_scrapy',
       description=DESCRIPTION,
+      author="ScrapeOps",
+      author_email="info@scrapeops.io",
       version=VERSION,
+      license="BSD",
+      url="https://github.com/ScrapeOps/scrapeops-scrapy-sdk",
       packages=find_packages(),
       install_requires=[
           "tld==0.12.4",
@@ -15,5 +19,17 @@ setup(name='scrapeops_scrapy',
           "json5==0.9.5",
           "urllib3==1.25.10",
           "uuid==1.30",
-          ]
+          ],
+      classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Intended Audience :: Developers",
+      ],
+      python_requires=">=3.6",
       )
