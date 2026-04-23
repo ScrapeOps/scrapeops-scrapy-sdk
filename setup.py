bea34b5ba789f2df3139b131ea8905ff816a452e
Joe Kearney <josephkearney91@gmail.com>
2026-04-23 16:56:47 +0200

    [Version update]

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 
-VERSION = '0.5.6'
+VERSION = '0.5.7'
 DESCRIPTION = 'Scrapeops Scrapy SDK, is a monitoring tool for your Scrapy spiders.'
 
 setup(name='scrapeops_scrapy',
